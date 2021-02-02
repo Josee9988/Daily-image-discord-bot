16904280fe30d21767024b6d1f6cd60c92daeb31
Josee9988 <jgracia9988@gmail.com>
2021-02-02 22:59:30 +0100

    "basic help command structure file"

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 require('dotenv').config();
+
+const {Client} = require('discord.js');
+const client = new Client();
+const PREFIX = "$";
+
+client.on('message', (message) => {
+    if (message.author.bot) return; //ignore bot messages
+    if (message.content.startsWith(PREFIX)){
+        const [CMD_NAME, ...args] = message.content
+            .trim()
+            .substring(PREFIX.length)
+            .split(/\s+/)
+
+
+        if (CMD_NAME === 'help'){
+            message.channel.send('This is my help message.');
+        }
+
+    }
+});
+
+client.login(process.env.DISCORDJS_BOT_TOKEN);
