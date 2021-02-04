927d2f843950d4ae2a45d0d2c6c91d88f0d8aa50
Josee9988 <jgracia9988@gmail.com>
2021-02-04 15:41:36 +0100

    "cron works"

---
+++ src/command-messages-data.ts
@@ -0,0 +1,36 @@
+export const helpMessage =
+    {
+        msg: `    -------------------------------------------------------------------------------------
+\`\`\`ini
+[daily Image Bot has been summoned here beep boop bep bep ⚡ ⚡ ]
+\`\`\`   
+**daily Image Bot** :robot:
+The prefix of the bot is: **\`!dimg\`**
+
+**Command list**:fire: :
+**\`help\`** **\`info\`** **\`channel\`** **\`albumlink\`** **\`ping\`** **\`pong\`**
+
+This project is open source, and we will only store the channel id/server id and your album link.
+For more information please visit *daily Image Bot Github repository*: https://github.com/Josee9988/daily-image-discord-bot
+\`\`\`diff
++ Closing connection with dimg💔 
+\`\`\``
+    };
+
+export const infoMessage =
+    {
+        msg: `**dimg info**
+
+**Contact the creator of the bot**:
+LinkedIn: https://www.linkedin.com/in/jose-gracia/
+GitHub: https://github.com/Josee9988
+Email: jgracia9988@gmail.com
+Personal page: http://jgracia.es/`
+    };
+
+
+export const sendRandomPhotoMessage =
+    {
+        msg1: "Here's your pic LOL",
+        msg2: "The photo was taken on the day: "
+    };
