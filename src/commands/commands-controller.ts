96b6d530d55f4e1bd424bbebfe4fa68d1b7e27a6
Josee9988 <jgracia9988@gmail.com>
2021-06-21 15:15:14 +0200

    "implemented the logic for the new message"

--- src/commands/commands-controller.ts
+++ src/commands/commands-controller.ts
@@ -157,6 +157,7 @@ export default class CommandsController {
      */
     private async fetchAndSendPhoto(dimg: IDimg): Promise<void> {
         let isDetectedAFetchFail: boolean = false;
+        let msg1Detected = dimg.sendMsg ? dimg.sendMsg : sendRandomPhotoMessage.msg1;
         if (dimg.albumLink === null || dimg.albumLink === undefined || dimg.albumLink.length < 20) return;
         const photos: ImageInfo[] | any = await GooglePhotosAlbum.fetchImageUrls(dimg.albumLink).catch(() => isDetectedAFetchFail = true);
         if (!photos || isDetectedAFetchFail) {
@@ -172,6 +173,7 @@ export default class CommandsController {
             }
             await this.client.channels.cache.get(dimg.channelId)
                 .send(photos[randomPhoto].url).catch((e: any) => console.error(`Couldn't send photo ${photos[randomPhoto]} with url ${photos[randomPhoto].url}.\nE: ${e}`));
+            await this.client.channels.cache.get(dimg.channelId).send(`${msg1Detected}`);
             await this.client.channels.cache.get(dimg.channelId)
                 .send(`${sendRandomPhotoMessage.msg2}**${new Date(photos[randomPhoto].imageUpdateDate).toLocaleDateString()}**`);
         }
